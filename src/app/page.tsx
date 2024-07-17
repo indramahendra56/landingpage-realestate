923ef96605f1ee5db1ab282ab60bdbebc42aeacb
indramahendra56 <indramahendra56@gmail.com>
2024-07-17 15:09:37 +0700

    feat(): landing-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,7 @@ export default function Home() {
   return (
     <main>
     <div>
-    <Button>
-      me
-    </Button>
-    </div>
-    <Button>tes</Button>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, magnam deleniti explicabo qui perferendis rem totam praesentium laudantium dolores iste porro corporis expedita ab quidem ratione quia nisi hic reiciendis!</p>
     </main>
     
   );
